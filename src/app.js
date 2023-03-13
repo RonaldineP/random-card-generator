5d99f46cb650454019d957d69075aa385796fb32
RonaldineP <ronaldinephanor@gmail.com>
2023-03-13 17:24:52 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").innerHTML = genRandNom();
+  document.querySelector(".card").classList.add([genRanSuits()]);
 };
+
+function genRandNom() {
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let random = Math.floor(Math.random() * num.length);
+  return num[random];
+}
+function genRanSuits() {
+  let suits = ["spade", "clubs", "heart", "diamonds"];
+  let random = Math.floor(Math.random() * suits.length);
+  return suits[random];
+}
